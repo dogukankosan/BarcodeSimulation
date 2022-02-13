1cfd19d77a721b64bc0174766ed10167c125be3e
dogukankosan <77827358+dogukankosan@users.noreply.github.com>
2022-02-13 20:03:29 +0300

    BarcodeSimulation
    Ömer Çolakoğlu Turkcel Geleceği Yazanlar video serisinde AD HOC QUERY  mantığı ve Stored Procedure anlattığı bir proje örneği

---
+++ Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace BarcodControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBarcode = new System.Windows.Forms.TextBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.White;
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.lblMsg);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtBarcode);
+            this.groupBox1.Location = new System.Drawing.Point(73, 80);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(367, 415);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button3.Location = new System.Drawing.Point(25, 113);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(139, 78);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Gönder(SP)(F3)";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblMsg.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.lblMsg.Location = new System.Drawing.Point(21, 228);
+            this.lblMsg.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(21, 20);
+            this.lblMsg.TabIndex = 3;
+            this.lblMsg.Text = "...";
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button2.Location = new System.Drawing.Point(230, 113);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 78);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Gönder(F2)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(21, 34);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Barkod Okutunuz:";
+            // 
+            // txtBarcode
+            // 
+            this.txtBarcode.Location = new System.Drawing.Point(25, 64);
+            this.txtBarcode.Margin = new System.Windows.Forms.Padding(4);
+            this.txtBarcode.Name = "txtBarcode";
+            this.txtBarcode.Size = new System.Drawing.Size(308, 22);
+            this.txtBarcode.TabIndex = 0;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox4.Image = global::BarcodControl.Properties.Resources.hh;
+            this.pictureBox4.Location = new System.Drawing.Point(349, 34);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(42, 19);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 5;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox3.Image = global::BarcodControl.Properties.Resources.f3;
+            this.pictureBox3.Location = new System.Drawing.Point(329, 554);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(37, 19);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox2.Image = global::BarcodControl.Properties.Resources.f2;
+            this.pictureBox2.Location = new System.Drawing.Point(145, 554);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(37, 19);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click_1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::BarcodControl.Properties.Resources.elTerminali21;
+            this.pictureBox1.Location = new System.Drawing.Point(4, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(515, 631);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lime;
+            this.ClientSize = new System.Drawing.Size(515, 628);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.Lime;
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblMsg;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBarcode;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace BarcodControl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        readonly string cs = "DATA SOURCE=DOGUKAN\\SQLSERVER;INITIAL CATALOG=PRODUCTION;UID=sa;PWD=Etasql1";
+        readonly SqlConnection conn = new SqlConnection();
+        readonly SqlCommand cmd = new SqlCommand();
+        readonly SqlDataAdapter da = new SqlDataAdapter();
+        readonly DataSet ds = new DataSet();
+        private bool down = false;
+        private int x, y;
+        void F2Click()
+        {
+            conn.ConnectionString = cs;
+            string sql = "SELECT * FROM PRODUCTION.DBO.BARCODES WHERE BARCODE='" + txtBarcode.Text + "'";
+            cmd.Connection = conn;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = sql;
+            da.SelectCommand = cmd;
+            ds.Clear();
+            ds.Reset();
+            da.Fill(ds);
+            if (ds.Tables[0].Rows.Count == 0)
+            {
+                lblMsg.Text = "Böyle bir barkod bulunamadı !";
+                return;
+            }
+            sql = "SELECT * FROM PRODUCTION.DBO.BARCODE_PRODUCTION WHERE BARCODE='" + txtBarcode.Text + "'";
+            cmd.Connection = conn;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = sql;
+            da.SelectCommand = cmd;
+            ds.Clear();
+            ds.Reset();
+            da.Fill(ds);
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                lblMsg.Text = "Bu barkod daha önceden okutulmuş !";
+                return;
+            }
+            sql = "SELECT * FROM PRODUCTION.DBO.PRODPLAN WHERE ITEMID IN (SELECT ITEMID FROM BARCODES WHERE BARCODE='" + txtBarcode.Text + "')";
+            cmd.Connection = conn;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = sql;
+            da.SelectCommand = cmd;
+            ds.Clear();
+            ds.Reset();
+            da.Fill(ds);
+            if (ds.Tables[0].Rows.Count == 0)
+            {
+                lblMsg.Text = "Bu ürüne ait üretim planı bulunamadı !";
+                return;
+            }
+            sql = "INSERT INTO PRODUCTION.DBO.BARCODE_PRODUCTION (BARCODE,DATE_,STATION) VALUES ('" + txtBarcode.Text + "',GETDATE(),1)";
+            cmd.Connection = conn;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = sql;
+            da.SelectCommand = cmd;
+            ds.Clear();
+            ds.Reset();
+            da.Fill(ds);
+            lblMsg.Text = "Barkod başarıyla eklendi.";
+            txtBarcode.Text = "";
+            txtBarcode.Focus();
+        }
+        void F3Click()
+        {
+            try
+            {
+                conn.ConnectionString = cs;
+                string sql = "EXEC BARCODE_INSERT @BARCODE='" + txtBarcode.Text + "',@STATIONID=1";
+                cmd.Connection = conn;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = sql;
+                da.SelectCommand = cmd;
+                ds.Clear();
+                ds.Reset();
+                da.Fill(ds);
+                lblMsg.Text = "Barkod başarıyla eklendi.";
+                txtBarcode.Text = "";
+                txtBarcode.Focus();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Bilgi Mesajı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            F2Click();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            F3Click();
+        }
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (down)
+            {
+                this.SetDesktopLocation(MousePosition.X - x, MousePosition.Y - y);
+            }
+        }
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            down = false;
+        }
+        private void pictureBox2_Click_1(object sender, EventArgs e)
+        {
+            F2Click();
+        }
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            F3Click();
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F2)
+                F2Click();
+            else if (e.KeyCode == Keys.F3)
+                F3Click();
+            else if (e.KeyCode==Keys.Escape)
+            Application.Exit();
+        }
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            down = true;
+            x = e.X;
+            y = e.Y;
+        }
+    }
+}
